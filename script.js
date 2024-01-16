e5636c172d725f9f983b8fabe74f5b52d4ee16e0
Muhammad Saqib <syedsaqib027@gmail.com>
2024-01-16 18:24:50 +0500

    Logical-Operators-JS

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 var x = "variable";
 var y = 42;
 document.write(x)
+document.write("<br>")
+
+
+
+
+// if Condition
+var a = 100;
+var b = 40;
+if (a > b){
+    document.write("if condition is true this will be print")
+    document.write("<br>")
+}
+// Logical Operators
+// Logical Operator AND(&&)
+var a = 40;
+var b = 29;
+var c = 83;
+var d = 82;
+if (a > b && c > d){
+    document.write("if both conditions are true this will be print")
+    document.write("<br>")
+}
+// Logical Operator OR(||)
+var a = 65;
+var b = 978;
+var c = 24;
+var d = 75;
+if (a > d || b > c){
+    document.write("if any of the condition is true this will be print")
+    document.write("<br>")
+}
+// Logical Operator NOT(!)
+var a = 65;
+var b = 978;
+if (!a < b){
+    document.write("if condition is false this will be print")
+}
