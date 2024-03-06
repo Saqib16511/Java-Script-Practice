abb5dc17fdb2b14205d85aef8f273cf3b4563ef2
Muhammad Saqib <syedsaqib027@gmail.com>
2024-03-06 19:18:35 +0500

    MULTI-SELECT-CHECKBOX-SESSION-STORAGE-JS

--- Assignment-pra/session/scriopt.js
+++ Assignment-pra/session/scriopt.js
@@ -1,15 +1,43 @@
-// let Q01 = document.getElementById("Q01");
-// let select = document.getElementsByName("Qno1")
-// let submit = document.getElementById("submit");
 
-// let select = document.querySelector('input[name="Qno1"]:checked').value;
+
+// QNO.01
+let select = document.getElementsByName('Qno1');
+let answers = "";
+let qno1 = document.getElementById("Q.01");
+let hello = qno1.getAttribute('id');
+
+// QNO.02
+let ques2 = document.getElementsByName("Qno2");
+let secanswer = "";
+let qno2 = document.getElementById("Q.02");
+let hii =qno2.getAttribute('id');
+
+// QNO.03
+let question3 = document.getElementsByName("Qno3"); 
+let ans3 = "";
+let qno3 = document.getElementById("Q.03");
+let result = qno3.getAttribute("id");
+
 submit.onclick = ()=>{
-        var answers = {};
-        var questions = document.getElementsByTagName('input');
-        for (var i = 0; i < questions.length; i++) {
-          if (questions[i].type === 'radio' && questions[i].checked){
-            answers[questions[i].name] = questions[i].value;
-        }
+    // Qno.01
+   for(i = 0;i < select.length;i++){
+    if(select[i].checked ){
+        answers += select[i].value;
+        sessionStorage.setItem(hello, JSON.stringify(answers))
     }
-        sessionStorage.setItem('mcqAnswers', JSON.stringify(answers));
+   }   
+// QNO.02
+   for(a = 0;a < ques2.length;a++){
+    if(ques2[a].checked){
+        secanswer += ques2[a].value;
+        sessionStorage.setItem(hii, JSON.stringify(secanswer)); 
+    }
+   }
+//    QNO03
+   for(j = 0;j < question3.length;j++){
+    if(question3[j].checked){
+        ans3 += question3[j].value;
+        sessionStorage.setItem(result, JSON.stringify(ans3))
+    }
+   }
 }
